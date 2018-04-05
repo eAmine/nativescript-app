2cee7b05ae1220912bb8641e07c09b7b13fd1160
Amine EL MALLEM <amine.elmallem@prima-solutions.com>
2018-04-05 17:45:12 +0200

    Claim Form first page

--- app/claim/claim.component.ts
+++ app/claim/claim.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {DrawerTransitionBase, SlideInOnTopTransition} from "nativescript-ui-sidedrawer";
 import {RadSideDrawerComponent} from "nativescript-ui-sidedrawer/angular";
+import {ClaimEvent} from "./model/ClaimEvent";
+import {Nature} from "./model/nature";
 
 @Component({
     selector: "Claim",
@@ -13,6 +15,21 @@ export class ClaimComponent implements OnInit {
     * It is used in the "onDrawerButtonTap" function below to manipulate the drawer.
     *************************************************************/
     @ViewChild("drawer") drawerComponent: RadSideDrawerComponent;
+    private _natures: Array<Nature>;
+    private _natureNames: Array<string>;
+
+    get natureNames() {
+        if (!this._natureNames) {
+            this._natureNames = this._natures.map((value: Nature) => value.name);
+        }
+        return this._natureNames;
+    }
+
+    private _claimEvent: ClaimEvent;
+
+    get claimEvent(): ClaimEvent {
+        return this._claimEvent;
+    }
 
     private _sideDrawerTransition: DrawerTransitionBase;
 
@@ -25,6 +42,20 @@ export class ClaimComponent implements OnInit {
     *************************************************************/
     ngOnInit(): void {
         this._sideDrawerTransition = new SlideInOnTopTransition();
+        this._claimEvent = new ClaimEvent();
+        this._claimEvent.natureCode = null;
+        this._claimEvent.lossDate = "";
+        this._claimEvent.lossTime = "";
+        this._claimEvent.description = "";
+        this._claimEvent.circumstance = "";
+        this._claimEvent.isThirdPartyPresent = false;
+        this._natures = new Array<Nature>();
+        this._natures.push(new Nature(1, "Accident"));
+        this._natures.push(new Nature(2, "Vol et vandalisme"));
+        this._natures.push(new Nature(3, "Incendie"));
+        this._natures.push(new Nature(4, "Bris de Glace"));
+        this._natures.push(new Nature(5, "Autre"));
+
     }
 
     /* ***********************************************************
@@ -33,5 +64,16 @@ export class ClaimComponent implements OnInit {
     *************************************************************/
     onDrawerButtonTap(): void {
         this.drawerComponent.sideDrawer.showDrawer();
+    }
+
+    onTap() {
+        //this.myCommitDataFormComp.dataForm.commitAll();
+
+        alert(
+            {
+                title: "Registration Details",
+                message: this._claimEvent.toString(),
+                okButtonText: "OK"
+            });
     }
 }
